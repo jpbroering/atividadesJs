b7fe7ea943c024255ce50f48b8969ee32a0d1f05
jpbroering <jpbroeringg@gmail.com>
2023-07-13 07:37:34 -0300

    criado  o repositorio

--- script/listTelefone.js
+++ script/listTelefone.js
@@ -1,3 +1,22 @@
+const listaT = [
+    {
+    nome: "joao paulo",
+    telefone: "912036482"
+    },
+    {
+    nome: "roberto",
+    telefone: "912375696"
+    },
+    {
+    nome: "raul",
+    telefone: "947632543"
+    },
+]
 function telefone(){
-    
+    let name = document.getElementById("nomeTelefone").value
+    for(n in listaT){
+        if(listaT[n].nome == name){
+            document.getElementById("telefoneText").innerHTML = listaT[n].telefone
+        }
+    }
 }
